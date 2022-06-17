d2ffd19507a928b2d24135ae2ecd08a6368bedb8
kellsaro <kellsaro@gmail.com>
2022-06-17 12:55:07 -0500

    Init commit for util gem

---
+++ lib/generators/util/service_generator.rb
@@ -0,0 +1,35 @@
+module Util
+  class ServiceGenerator < Rails::Generators::NamedBase
+    source_root File.expand_path("templates", __dir__)
+
+    class_option :module, type: :string
+
+    def create_service_file
+      @module_name = options[:module]
+
+      @spaces = @module_name.present? ? "  " : ""
+      service_dir_path = "app/services"
+      generator_dir_path = service_dir_path + ("/#{@module_name.underscore}" if @module_name.present?).to_s
+      generator_path = generator_dir_path + "/#{file_name}_service.rb"
+
+      Dir.mkdir(service_dir_path) unless File.exist?(service_dir_path)
+      Dir.mkdir(generator_dir_path) unless File.exist?(generator_dir_path)
+
+      template "service_template.erb", generator_path
+    end
+
+    def create_spec_file
+      @module_name = options[:module]
+
+      @spaces = @module_name.present? ? "  " : ""
+      service_dir_path = "spec/services"
+      generator_dir_path = service_dir_path + ("/#{@module_name.underscore}" if @module_name.present?).to_s
+      generator_path = generator_dir_path + "/#{file_name}_service_spec.rb"
+
+      Dir.mkdir(service_dir_path) unless File.exist?(service_dir_path)
+      Dir.mkdir(generator_dir_path) unless File.exist?(generator_dir_path)
+
+      template "service_template_spec.erb", generator_path
+    end
+  end
+end
